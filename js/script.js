261e3821b898186e36d7b2820040167764eb987b
hasna Chaddou <hasnachad@gmail.com>
2017-01-10 22:08:02 +0100

    fin de l'exercice

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
-
+/* Etape 3 : Je veux 200 cases de 1 à 200.
 const SEUIL = 200;
 
 for (var i = 1; i <= SEUIL; i++) {
 	document.getElementById('liste').innerHTML += '<li>'+i+'</li>';
 	alert();
 }
+*/
 
+var seuil=prompt("combien de case voulez-vous?");
+if (seuil<1000) {
+for (var i = 1; i <= seuil; i++) {
+	document.getElementById('liste').innerHTML += '<li>'+i+'</li>';
+	
+}}else{
+	alert('Mauvaise saisie');/*empêche l'utilisateur */
+}
 
 	
